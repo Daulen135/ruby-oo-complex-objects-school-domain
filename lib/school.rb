311f432c7911f77716744a520858653657cf01bf
Daulen135 <daulenz@gmail.com>
2021-02-28 22:57:24 -0600

    commit tree b24d41157ee6601e90c9d6d04164bd83bbfcbb41

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,36 @@
-# code here!
+require "pry"
+
+class School
+    
+    attr_accessor :name,:roster
+    
+    def initialize (name)
+        @name=name
+        @roster={}
+    end
+
+    def name
+        @name
+    end
+
+    def add_student(student_name,grade)
+        if @roster[grade]
+            @roster[grade]<<student_name
+
+        else 
+            @roster[grade]=[student_name]
+        end
+    end
+
+    def grade(grade) 
+        @roster[grade]
+    end
+
+    def sort
+        sorted_hash={}
+        @roster.each do|grade,name_array|
+            sorted_hash[grade]=(name_array.sort)
+        end
+        sorted_hash
+    end
+end
